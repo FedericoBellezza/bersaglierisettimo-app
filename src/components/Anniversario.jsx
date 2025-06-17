ef5b699addfbe313805058e4dcc1219bd19a50b0
Federico Bellezza <federico.bellezza2001@gmail.com>
2025-06-17 09:47:39 +0200

    aggiunta logo 30esimo

--- src/components/Anniversario.jsx
+++ src/components/Anniversario.jsx
@@ -50,7 +50,7 @@ export default function Anniversario() {
               variants={itemVariants}
               className="flex justify-center mb-6"
             >
-              <div className="bg-red-600 text-white rounded-full px-6 py-2 text-lg font-bold">
+              <div className="bg-red-600 text-white rounded-full px-6 py-2 text-md font-bold">
                 1995 - 2025
               </div>
             </motion.div>
@@ -66,9 +66,9 @@ export default function Anniversario() {
                   className="absolute -bottom-2 left-0 h-1 bg-red-600"
                 ></motion.span>
               </h2>
-              <p className="text-lg text-gray-600">
+              {/* <p className="text-lg text-gray-600">
                 Celebriamo tre decenni di storia, musica e tradizione
-              </p>
+              </p> */}
             </motion.div>
 
             {/* Contenuto principale */}
@@ -79,16 +79,8 @@ export default function Anniversario() {
               {/* Immagine o logo */}
               <div className="md:w-1/3 flex justify-center">
                 <div className="relative">
-                  <div className="absolute inset-0 bg-red-600 rounded-full blur-sm opacity-20"></div>
-                  <div className="bg-white rounded-full p-4 shadow-lg relative">
-                    <div className="bg-gradient-to-r from-red-500  to-orange-500 rounded-full text-center px-8 py-6 aspect-square">
-                      <span className="block text-3xl font-bold text-white">
-                        30
-                      </span>
-                      <span className="block text-lg font-bold text-white">
-                        ANNI
-                      </span>
-                    </div>
+                  <div className=" rounded-full p-4  relative">
+                    <img src="./logo-30esimo.png" alt="" />
                   </div>
                 </div>
               </div>
