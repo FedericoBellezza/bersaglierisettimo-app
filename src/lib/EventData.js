5247a1b1c580cfb9abec740f66ca3dfb3d7d2918
FedericoBellezza21 <federico.bellezza2001@gmail.com>
2025-07-26 17:18:14 +0200

    Update event images and add new carosello.avif

--- src/lib/EventData.js
+++ src/lib/EventData.js
@@ -7,8 +7,7 @@ const events = [
     year: "2025",
     location: "Villastellone",
     description: "Concerto in occasione del raduno provincia di Torino.",
-    image:
-      "https://static.wixstatic.com/media/a01c56_45a916c98f9142ca8ea1b1d9fbae9e1c~mv2.jpg/v1/fill/w_953,h_378,al_c,q_85,usm_0.66_1.00_0.01,enc_avif/a01c56_45a916c98f9142ca8ea1b1d9fbae9e1c~mv2.jpg",
+    image: "hero-image.avif",
     type: "Concerto",
     startTime: "21:00",
     endTime: "23:00",
@@ -21,8 +20,7 @@ const events = [
     year: "2025",
     location: "Villastellone",
     description: "Raduno provincia di Torino.",
-    image:
-      "https://static.wixstatic.com/media/a01c56_fb44292d22434cc58c44181e6d5fec1b~mv2.jpg/v1/fill/w_953,h_378,al_c,q_85,usm_0.66_1.00_0.01,enc_avif/a01c56_fb44292d22434cc58c44181e6d5fec1b~mv2.jpg",
+    image: "sfilata.avif",
     type: "Raduno",
     startTime: "09:00",
     endTime: "16:00",
@@ -35,8 +33,7 @@ const events = [
     year: "2025",
     location: "Pala200, Via Santa Cristina 5, Settimo Torinese",
     description: "30° anniversario rifondazione fanfara di Settimo Torinese.",
-    image:
-      "https://static.wixstatic.com/media/a01c56_7cc5ad117fdf4202b8ff9054039e924b~mv2.png/v1/fill/w_795,h_315,al_c,q_85,enc_avif/a01c56_7cc5ad117fdf4202b8ff9054039e924b~mv2.png",
+    image: "carosello.avif",
     type: "Serata musicale",
     startTime: "21:00",
     endTime: null,
@@ -49,8 +46,7 @@ const events = [
     year: "2025",
     location: "Settimo Torinese",
     description: "Sfilata per le vie della città di Settimo Torinese.",
-    image:
-      "https://static.wixstatic.com/media/a01c56_fb44292d22434cc58c44181e6d5fec1b~mv2.jpg/v1/fill/w_953,h_378,al_c,q_85,usm_0.66_1.00_0.01,enc_avif/a01c56_fb44292d22434cc58c44181e6d5fec1b~mv2.jpg",
+    image: "sfilata2.avif",
     type: "Sfilata",
     startTime: "09:30",
     endTime: "17:00",
